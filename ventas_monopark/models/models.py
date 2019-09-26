8d60b00b6e5a17bcf79099a76443b0d9e0aa6961
gilxmarts <gilberto.santiago@xmarts.com>
2019-09-26 11:16:53 -0500

    Pagos 7

--- ventas_monopark/models/models.py
+++ ventas_monopark/models/models.py
@@ -2,6 +2,18 @@
 
 from odoo import models, fields, api
 
+class pagos_pagos(models.Model):
+	_inherit = 'account.payment'
+
+	@api.model
+	def l10n_mx_edi_get_pago_etree(self, cfdi):
+		if not hasattr(cfdi, 'Complemento'):
+			return None
+		attribute = 'pago10:Pagos[1]'
+		namespace = {'pago10': 'http://www.sat.gob.mx/Pagos'}
+		node = cfdi.Complemento.xpath(attribute, namespaces=namespace)
+		return node[0] if node else None
+
 #MODELO PARA LOS TIEMPOS DE ENTREGA EN EL MODELO DE VENTAS
 class TiempoEntrega(models.Model):
 	_name = "tiempo.entrega"
